e59a8f522748049be9332366799a57c431d29804
toannhanb7 <toannhanb7@583ab0cc-dbc0-64e3-9ef5-10d94916411b>
2010-05-15 20:20:24 +0000

    Lại chờ nhóm data design sửa lại cấu trúc của file RTF phần tập đọc

---
+++ 8. Source Code/TiengViet4/DAO/TapDocDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace DAO
+{
+    public class TapDocDAO
+    {
+        public static DataTable LayDanhSachBai(string strMaBaiHoc)
+        {
+            string strCmd = "Select * From FileLuyenTap " +
+                    "Where MaBaiHoc = '" + strMaBaiHoc + "' " +
+                    "And MaLoaiMon = 1";
+            return CoSoDuLieu.LayDuLieu(strCmd);
+        }
+    }
+}
