7edb6e4a550609d904c371a9b403aae2145c9b77
toannhanb7 <toannhanb7@583ab0cc-dbc0-64e3-9ef5-10d94916411b>
2010-05-15 20:20:24 +0000

    Lại chờ nhóm data design sửa lại cấu trúc của file RTF phần tập đọc

---
+++ trunk/8. Source Code/TiengViet4/BUS/TapDocBUS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DAO;
+namespace BUS
+{
+    public class TapDocBUS
+    {
+        public static DataTable LayDanhSachBai(string strMaBaiHoc)
+        {
+            DataTable dtBaiHoc = new DataTable("BAIHOC");
+            dtBaiHoc = TapDocDAO.LayDanhSachBai(strMaBaiHoc);
+            return dtBaiHoc;
+        }
+    }
+}
---
+++ trunk/8. Source Code/TiengViet4/GUI/MusicPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace TiengViet4
+{
+    class MusicPlayer
+    {
+        string strCommand;
+        bool blnOpened;
+
+        [DllImport("Winmm.dll")]
+        private static extern long mciSendString(string strComand, string strReturn, int intReturnLength, IntPtr hwndCallback);
+
+        public void Open(string strFileName)
+        {
+            strCommand = "open \"" + strFileName + "\" type mpegvideo alias MediaFile";
+            mciSendString(strCommand, null, 0, IntPtr.Zero);
+            blnOpened = true;
+        }
+
+        public void Play(bool blnLoop)
+        {
+            if (blnOpened == true)
+            {
+                strCommand = "play MediaFile";
+                if (blnLoop == true)
+                {
+                    strCommand += " REPEAT";
+                }
+                mciSendString(strCommand, null, 0, IntPtr.Zero);
+            }
+        }
+
+        public void Close()
+        {
+            if (blnOpened == true)
+            {
+                strCommand = "close MediaFile";
+                mciSendString(strCommand, null, 0, IntPtr.Zero);
+                blnOpened = false;
+            }
+        }
+    }
+}
