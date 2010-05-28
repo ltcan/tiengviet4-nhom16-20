d1435ca6f82d8ab5676cdbd14fb61e3a61c62715
toannhanb7 <toannhanb7@583ab0cc-dbc0-64e3-9ef5-10d94916411b>
2010-05-28 15:04:00 +0000

    Xong phần tập đọc.

---
+++ trunk/8. Source Code/TiengViet4/DTO/TapDocDTO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class TapDocDTO
+    {
+        private string strMaBaiHoc;
+
+        public string MaBaiHoc
+        {
+            get { return strMaBaiHoc; }
+            set { strMaBaiHoc = value; }
+        }
+        private string strDuongDanFileNoiDung;
+
+        public string DuongDanFileNoiDung
+        {
+            get { return strDuongDanFileNoiDung; }
+            set { strDuongDanFileNoiDung = value; }
+        }
+        private string strDuongDanFileHinhAnh;
+
+        public string DuongDanFileHinhAnh
+        {
+            get { return strDuongDanFileHinhAnh; }
+            set { strDuongDanFileHinhAnh = value; }
+        }
+        private string strDuongDanFileCauHoi;
+
+        public string DuongDanFileCauHoi
+        {
+            get { return strDuongDanFileCauHoi; }
+            set { strDuongDanFileCauHoi = value; }
+        }
+        private string strDuongDanFileDapAn;
+
+        public string DuongDanFileDapAn
+        {
+            get { return strDuongDanFileDapAn; }
+            set { strDuongDanFileDapAn = value; }
+        }
+
+        public TapDocDTO()
+        {
+            DuongDanFileCauHoi = "";
+            DuongDanFileDapAn = "";
+            DuongDanFileHinhAnh = "";
+            DuongDanFileNoiDung = "";
+            MaBaiHoc = "";
+        }
+    }
+}
