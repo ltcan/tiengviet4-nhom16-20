b9542f69ac66764b03f24b74f043cfb5d1066328
lv.long.tn <lv.long.tn@583ab0cc-dbc0-64e3-9ef5-10d94916411b>
2010-05-30 17:55:19 +0000

    Chức năng giải trí

---
+++ 8. Source Code/TiengViet4/GUI/ChonGiaiTriForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace TiengViet4
+{
+    partial class ChonGiaiTriForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChonGiaiTriForm));
+            this.btnThanhDieuHuongDuoi = new DevComponents.DotNetBar.BubbleBar();
+            this.bubbleBarTab2 = new DevComponents.DotNetBar.BubbleBarTab(this.components);
+            this.btnHome = new DevComponents.DotNetBar.BubbleButton();
+            this.btnHuongDan = new DevComponents.DotNetBar.BubbleButton();
+            this.btnAmThanh = new DevComponents.DotNetBar.BubbleButton();
+            this.btnThoat = new DevComponents.DotNetBar.BubbleButton();
+            this.btnGame3 = new System.Windows.Forms.Button();
+            this.btnGame4 = new System.Windows.Forms.Button();
+            this.btnGame1 = new System.Windows.Forms.Button();
+            this.btnGame2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.btnThanhDieuHuongDuoi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThanhDieuHuongDuoi
+            // 
+            this.btnThanhDieuHuongDuoi.Alignment = DevComponents.DotNetBar.eBubbleButtonAlignment.Bottom;
+            this.btnThanhDieuHuongDuoi.AntiAlias = true;
+            this.btnThanhDieuHuongDuoi.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderBottomWidth = 1;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderLeftWidth = 1;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderRightWidth = 1;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.BorderTopWidth = 1;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.PaddingBottom = 3;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.PaddingLeft = 3;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.PaddingRight = 3;
+            this.btnThanhDieuHuongDuoi.ButtonBackAreaStyle.PaddingTop = 3;
+            this.btnThanhDieuHuongDuoi.ButtonBackgroundStretch = false;
+            this.btnThanhDieuHuongDuoi.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnThanhDieuHuongDuoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThanhDieuHuongDuoi.ImageSizeLarge = new System.Drawing.Size(86, 86);
+            this.btnThanhDieuHuongDuoi.ImageSizeNormal = new System.Drawing.Size(64, 64);
+            this.btnThanhDieuHuongDuoi.Location = new System.Drawing.Point(231, 525);
+            this.btnThanhDieuHuongDuoi.MouseOverTabColors.BorderColor = System.Drawing.Color.Transparent;
+            this.btnThanhDieuHuongDuoi.Name = "btnThanhDieuHuongDuoi";
+            this.btnThanhDieuHuongDuoi.SelectedTab = this.bubbleBarTab2;
+            this.btnThanhDieuHuongDuoi.SelectedTabColors.BorderColor = System.Drawing.Color.Transparent;
+            this.btnThanhDieuHuongDuoi.Size = new System.Drawing.Size(384, 76);
+            this.btnThanhDieuHuongDuoi.TabIndex = 15;
+            this.btnThanhDieuHuongDuoi.Tabs.Add(this.bubbleBarTab2);
+            this.btnThanhDieuHuongDuoi.TabsVisible = false;
+            this.btnThanhDieuHuongDuoi.Text = "bubbleBar2";
+            // 
+            // bubbleBarTab2
+            // 
+            this.bubbleBarTab2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(247)))));
+            this.bubbleBarTab2.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(168)))), ((int)(((byte)(228)))));
+            this.bubbleBarTab2.Buttons.AddRange(new DevComponents.DotNetBar.BubbleButton[] {
+            this.btnHome,
+            this.btnHuongDan,
+            this.btnAmThanh,
+            this.btnThoat});
+            this.bubbleBarTab2.DarkBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.bubbleBarTab2.LightBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.bubbleBarTab2.Name = "bubbleBarTab2";
+            this.bubbleBarTab2.PredefinedColor = DevComponents.DotNetBar.eTabItemColor.Blue;
+            this.bubbleBarTab2.Text = "btThanhDieuHuongDuoi";
+            this.bubbleBarTab2.TextColor = System.Drawing.Color.Black;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.ImageLarge = ((System.Drawing.Image)(resources.GetObject("btnHome.ImageLarge")));
+            this.btnHome.Name = "btnHome";
+            this.btnHome.TooltipText = "Về trang đầu";
+            this.btnHome.Click += new DevComponents.DotNetBar.ClickEventHandler(this.btnHome_Click);
+            // 
+            // btnHuongDan
+            // 
+            this.btnHuongDan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHuongDan.Image = ((System.Drawing.Image)(resources.GetObject("btnHuongDan.Image")));
+            this.btnHuongDan.ImageLarge = ((System.Drawing.Image)(resources.GetObject("btnHuongDan.ImageLarge")));
+            this.btnHuongDan.Name = "btnHuongDan";
+            this.btnHuongDan.TooltipText = "Thông tin trợ giúp";
+            this.btnHuongDan.Click += new DevComponents.DotNetBar.ClickEventHandler(this.btnHuongDan_Click);
+            // 
+            // btnAmThanh
+            // 
+            this.btnAmThanh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAmThanh.Image = ((System.Drawing.Image)(resources.GetObject("btnAmThanh.Image")));
+            this.btnAmThanh.ImageLarge = ((System.Drawing.Image)(resources.GetObject("btnAmThanh.ImageLarge")));
+            this.btnAmThanh.Name = "btnAmThanh";
+            this.btnAmThanh.TooltipText = "Bật/Tắt nhạc";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageLarge = ((System.Drawing.Image)(resources.GetObject("btnThoat.ImageLarge")));
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.TooltipText = "Thoát";
+            this.btnThoat.Click += new DevComponents.DotNetBar.ClickEventHandler(this.btnThoat_Click);
+            // 
+            // btnGame3
+            // 
+            this.btnGame3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGame3.BackgroundImage")));
+            this.btnGame3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnGame3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGame3.Location = new System.Drawing.Point(98, 305);
+            this.btnGame3.Name = "btnGame3";
+            this.btnGame3.Size = new System.Drawing.Size(169, 147);
+            this.btnGame3.TabIndex = 16;
+            this.btnGame3.UseVisualStyleBackColor = true;
+            this.btnGame3.Click += new System.EventHandler(this.btnGame3_Click);
+            // 
+            // btnGame4
+            // 
+            this.btnGame4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGame4.BackgroundImage")));
+            this.btnGame4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnGame4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGame4.Location = new System.Drawing.Point(532, 305);
+            this.btnGame4.Name = "btnGame4";
+            this.btnGame4.Size = new System.Drawing.Size(169, 147);
+            this.btnGame4.TabIndex = 16;
+            this.btnGame4.UseVisualStyleBackColor = true;
+            this.btnGame4.Click += new System.EventHandler(this.btnGame4_Click);
+            // 
+            // btnGame1
+            // 
+            this.btnGame1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGame1.BackgroundImage")));
+            this.btnGame1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGame1.Location = new System.Drawing.Point(98, 76);
+            this.btnGame1.Name = "btnGame1";
+            this.btnGame1.Size = new System.Drawing.Size(169, 147);
+            this.btnGame1.TabIndex = 16;
+            this.btnGame1.UseVisualStyleBackColor = true;
+            this.btnGame1.Click += new System.EventHandler(this.btnGame1_Click);
+            // 
+            // btnGame2
+            // 
+            this.btnGame2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGame2.BackgroundImage")));
+            this.btnGame2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGame2.Location = new System.Drawing.Point(532, 76);
+            this.btnGame2.Name = "btnGame2";
+            this.btnGame2.Size = new System.Drawing.Size(169, 147);
+            this.btnGame2.TabIndex = 16;
+            this.btnGame2.UseVisualStyleBackColor = true;
+            this.btnGame2.Click += new System.EventHandler(this.btnGame2_Click);
+            // 
+            // ChonGiaiTriForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TiengViet4.Properties.Resources._5Bwallcoo51;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.btnGame2);
+            this.Controls.Add(this.btnGame4);
+            this.Controls.Add(this.btnGame1);
+            this.Controls.Add(this.btnGame3);
+            this.Controls.Add(this.btnThanhDieuHuongDuoi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ChonGiaiTriForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChonGiaiTriForm";
+            ((System.ComponentModel.ISupportInitialize)(this.btnThanhDieuHuongDuoi)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.BubbleBar btnThanhDieuHuongDuoi;
+        private DevComponents.DotNetBar.BubbleBarTab bubbleBarTab2;
+        private DevComponents.DotNetBar.BubbleButton btnHome;
+        private DevComponents.DotNetBar.BubbleButton btnHuongDan;
+        private DevComponents.DotNetBar.BubbleButton btnAmThanh;
+        private DevComponents.DotNetBar.BubbleButton btnThoat;
+        private System.Windows.Forms.Button btnGame3;
+        private System.Windows.Forms.Button btnGame4;
+        private System.Windows.Forms.Button btnGame1;
+        private System.Windows.Forms.Button btnGame2;
+    }
+}
---
+++ 8. Source Code/TiengViet4/GUI/Logic.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace TiengViet4
+{
+    partial class Logic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Logic));
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(-1, 0);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(796, 567);
+            this.axShockwaveFlash1.TabIndex = 0;
+            // 
+            // Logic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 566);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Logic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logic";
+            this.Load += new System.EventHandler(this.Logic_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+    }
+}
