7fc2ebf934b61f4ef27ae3c4996aa0e441ca6c2d
lv.long.tn <lv.long.tn@583ab0cc-dbc0-64e3-9ef5-10d94916411b>
2010-06-05 14:48:14 +0000

    Thêm màn hình ôn tập

---
+++ trunk/8. Source Code/TiengViet4/GUI/OnTap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TiengViet4
+{
+    public partial class OnTap : Form
+    {
+        public OnTap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, DevComponents.DotNetBar.ClickEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnVeTrangDau_Click(object sender, DevComponents.DotNetBar.ClickEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
